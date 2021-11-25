c769b4710dd15d94b60f2c0380ff757543428347
maize-j <1522843099@qq.com>
2021-11-25 17:37:30 +0800

    配置中心&消息总线

--- cloud-config-center3355/src/main/java/cn/yz/springcloud/controller/ConfigClientController.java
+++ cloud-config-center3355/src/main/java/cn/yz/springcloud/controller/ConfigClientController.java
@@ -13,13 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/9/15 12:18
  */
 @RestController
+//刷新，每次github修改后，3355获取到的都是最新的
+//添加这个注解后，直接刷新还是不会生效的，需要运维人员在修改配置后发送post请求刷新3355
+//curl -X POST "http://localhost:3355/actuator/refresh"
+//想要完全实现自动更新要配合消息总线bus
 @RefreshScope
 public class ConfigClientController {
     @Value("${config.info}")  //从GitHub上的http://config-3344.com:3344/master/config-dev.yml读取config.info的内容
     private String configInfo;
+//
+//    @Value("${config}")
+//    private String config;
 
     @GetMapping("/configInfo")
     public String getConfigInfo(){
         return configInfo;
     }
+
+//    @GetMapping("/config")
+//    public String getConfig(){
+//        return config;
+//    }
 }
---
+++ cloud-config-center3366/src/main/java/cn/yz/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package cn.yz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * TODO
+ *
+ * @author 苞谷洁子
+ * @ClassName cn.yz.springcloud.ConfigClientMain3366
+ * @date 2021/11/25 16:48
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
